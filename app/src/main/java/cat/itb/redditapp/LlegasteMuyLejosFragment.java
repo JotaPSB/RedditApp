08922ee06d669e569912cbec17e4fab0b74c9f36
Aaron <aaron.martinez.7e3@itb.cat>
2021-03-03 21:07:57 +0100

    Creación de fragment para rellenar y Bottom Navigation menu

--- app/src/main/java/cat/itb/redditapp/LlegasteMuyLejosFragment.java
+++ app/src/main/java/cat/itb/redditapp/LlegasteMuyLejosFragment.java
@@ -5,24 +5,20 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+public class LlegasteMuyLejosFragment extends Fragment {
 
-
-public class BlankFragment extends Fragment {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-           }
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank, container, false);
+        return inflater.inflate(R.layout.fragment_llegaste_muy_lejos, container, false);
     }
-
 }
--- app/src/main/java/cat/itb/redditapp/MainActivity.java
+++ app/src/main/java/cat/itb/redditapp/MainActivity.java
@@ -1,15 +1,18 @@
 package cat.itb.redditapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.appbar.MaterialToolbar;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.tabs.TabLayout;
 
 import cat.itb.redditapp.adapter.ViewPagerAdapter;
@@ -22,19 +25,26 @@ public class MainActivity extends AppCompatActivity {
     private ViewPagerAdapter adapter;
     private DrawerLayout drawerLayout;
     private MaterialToolbar topAppBar;
+    private BottomNavigationView bottomNavigationView;
+    private ChatFragment chatFragment = new ChatFragment();
+    private LlegasteMuyLejosFragment lejosFragment = new LlegasteMuyLejosFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.nav_drawer);
 
         tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
         viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
         topAppBar = findViewById(R.id.top_app_bar);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         adapter.AddFragment(new BlankFragment(),"Home");
         adapter.AddFragment(new BlankFragment(),"Popular");
+
         topAppBar.setNavigationOnClickListener(new MaterialToolbar.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +54,49 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
 
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.page_1:
+                        visibilidadOn();
+                        return true;
 
+                    case R.id.page_3:
+                        visibilidadOn();
+                        return true;
+
+                    case R.id.page_2:
+                        visibilidadOff();
+                        changeFragment(lejosFragment);
+                        return true;
+
+                    case R.id.page_4:
+                        visibilidadOff();
+                        changeFragment(chatFragment);
+
+                    case R.id.page_5:
+
+
+                    default:
+                        return true;
+                }
+            }
+        });
+    }
+
+    private void visibilidadOn(){
+        tabLayout.setVisibility(View.VISIBLE);
+        viewPager.setVisibility(View.VISIBLE);
+    }
+
+    private void visibilidadOff(){
+        tabLayout.setVisibility(View.GONE);
+        viewPager.setVisibility(View.GONE);
+    }
+
+    private void changeFragment(Fragment fragment){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
     }
 }
